852dc274981ae11c0f8304cb4af5561f5238c550
Zixian <109778851+zachczx@users.noreply.github.com>
2026-08-17 19:14:51 +0800

    chore: make slashless routes explicit

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,7 @@ import type { LayoutLoad } from './$types';
 
 export const prerender = true;
 export const ssr = true;
+export const trailingSlash = 'never';
 
 export const load: LayoutLoad = async ({ url }) => {
 	return {
